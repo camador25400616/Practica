f5e8611385b3768de6fc9123296a8bec38661b1a
Amador López César Rafael <ceraamadorlo@ittepic.edu.mx>
2026-03-19 22:05:24 -0600

    feat: clase Smartphone

--- SmartphonePOO/src/Smartphone.java
+++ SmartphonePOO/src/Smartphone.java
@@ -1,12 +1,78 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author 981022913
- */
 public class Smartphone {
+    private String numeroSerie;
+    private String marca;
+    private String modelo;
+    private Procesador procesador;
+    private Bateria bateria;
+    private SistemaOperativo sistemaOperativo;
     
+    public Smartphone(String numeroSerie, String marca, String modelo, Procesador procesador, Bateria bateria, SistemaOperativo sistemaOperativo){
+        this.numeroSerie = numeroSerie;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.procesador = procesador;
+        this.bateria = bateria;
+        this.sistemaOperativo = sistemaOperativo;
+    }
+    
+    public String getNumeroSerie(){
+        return this.numeroSerie;
+    }
+    
+    public String getMarca(){
+        return this.marca;
+    }
+    
+    public String getModelo(){
+        return this.modelo;
+    }
+    
+    public Procesador getProcesador(){
+        return this.procesador;
+    }
+    
+    public Bateria getBateria(){
+        return this.bateria;
+    }
+    
+    public SistemaOperativo getSistemaOperativo(){
+        return this.sistemaOperativo;
+    }
+    
+    public void setNumeroSerie(String numeroSerie){
+        this.numeroSerie = numeroSerie;
+    }
+    
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    
+    public void setModelo(String modelo){
+        this.modelo = modelo;
+    }
+    
+    public void setProcesador(Procesador procesador){
+        this.procesador = procesador;
+    }
+    
+    public void setBateria(Bateria bateria){
+        this.bateria = bateria;
+    }
+    
+    public void setSistemaOperativo(SistemaOperativo sistemaOperativo){
+        this.sistemaOperativo = sistemaOperativo;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("===Smartphone===").append("\n");
+        sb.append("Numero de serie = ").append(this.numeroSerie).append("\n");
+        sb.append("Marca = ").append(this.marca).append("\n");
+        sb.append("Modelo = ").append(this.modelo).append("\n");
+        sb.append("Procesador = ").append(this.procesador).append("\n");
+        sb.append("Bateria = ").append(this.bateria).append("\n");
+        sb.append("Sistema Operativo = ").append(this.sistemaOperativo).append("\n");
+        return sb.toString();
+    }
 }
