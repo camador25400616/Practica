0bea7e44b7e15adb1a8c49507fbe570e975e6906
Amador López César Rafael <ceraamadorlo@ittepic.edu.mx>
2026-03-19 21:12:38 -0600

    feat: clase procesador no vacia

--- SmartphonePOO/src/Procesador.java
+++ SmartphonePOO/src/Procesador.java
@@ -1,12 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author 981022913
- */
 public class Procesador {
+    private String marca;
+    private int nucleos;
+    private double frecuenciaGHz;
+
+    public Procesador(String marca, int nucleos, double frecuenciaGHz) {
+        this.marca = marca;
+        this.nucleos = nucleos;
+        this.frecuenciaGHz = frecuenciaGHz;
+    }
+
+    public Procesador() {
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public int getNucleos() {
+        return nucleos;
+    }
+
+    public double getFrecuenciaGHz() {
+        return frecuenciaGHz;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setNucleos(int nucleos) {
+        this.nucleos = nucleos;
+    }
+
+    public void setFrecuenciaGHz(double frecuenciaGHz) {
+        this.frecuenciaGHz = frecuenciaGHz;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("===Procesador===").append("\n");
+        sb.append("marca = ").append(marca).append("\n");
+        sb.append("nucleos = ").append(nucleos).append("\n");
+        sb.append(", frecuenciaGHz=").append(frecuenciaGHz).append("\n");
+        return sb.toString();
+    }
+    
     
 }
