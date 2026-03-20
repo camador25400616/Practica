ce82469c0b311eafa91b1efb6385256650d14665
Amador López César Rafael <ceraamadorlo@ittepic.edu.mx>
2026-03-19 21:23:57 -0600

    feat: clase bateria

---
+++ SmartphonePOO/src/Bateria.java
@@ -0,0 +1,34 @@
+public class Bateria {
+    private int capacidadMAh;
+    private String tipo;
+    
+    public Bateria(int capacidadMAh, String tipo){
+        this.capacidadMAh = capacidadMAh;
+        this.tipo = tipo;
+    }
+    
+    public int getCapacidadMAh(){
+        return this.capacidadMAh;
+    }
+    
+    public String getTipo(){
+        return this.tipo;
+    }
+    
+    public void setCapacidadMAh(int capacidadMAh){
+        this.capacidadMAh = capacidadMAh;
+    }
+    
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+    
+    @Override
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+        sb.append("===Bateria===").append("\n");
+        sb.append("Capacidad mAh = ").append(this.capacidadMAh);
+        sb.append("Tipo de bateria = ").append(this.tipo);
+        return sb.toString();
+    }
+}
